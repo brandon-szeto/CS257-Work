5d0ba63aead9fe1d9ec7df261ead253d5ae2e34e
Brandon Szeto <brandonszeto@Brandons-Virus.local>
2024-01-17 09:28:02 -0600

    Updated the createable.sql file to fit my data

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,12 +1,22 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
-  quakedate date,
   quaketime time with time zone,
   latitude real,
   longitude real,
   quakedepth real,
   mag real,
+  magType text,
+  nst text,
+  gap text,
+  dmin text,
+  rms text,
+  net text,
   id text,
   place text,
-  quaketype text
+  horizontalError text,
+  depthError text, 
+  magError text, 
+  magNst text,
+  locationSource text,
+  magSource text,
 );
